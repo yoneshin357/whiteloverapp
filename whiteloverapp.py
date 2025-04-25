3f69bd842f8e47bbc406d90b532899ab1c5babba
yoneshin357 <yoneshin357@gmail.com>
2025-04-25 18:55:51 +0900

    Update whiteloverapp.py

--- whiteloverapp.py
+++ whiteloverapp.py
@@ -13,6 +13,11 @@ def login_screen():
     st.header("This app is private.")
     st.subheader("Please log in.")
     st.button("Log in with Google", on_click=st.login)
+
+if not st.experimental_user.is_logged_in:
+    login_screen()
+else:
+    st.experimental_user
 ###ファイルパス設定（直下を参照する）
 path= ''
 
