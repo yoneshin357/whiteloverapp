428c7fb9a1a5e42f2cabd96dfa32e5203b52e05f
yoneshin357 <yoneshin357@gmail.com>
2025-05-15 19:41:47 +0900

    Update whiteloverapp.py

--- app/whiteloverapp.py
+++ app/whiteloverapp.py
@@ -15,6 +15,7 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from googleapiclient.http import MediaIoBaseUpload
+from googleapiclient.http import MediaIoBaseDownload
 
 
 
